ec50a1e09f7b199fad10daea7ede6b9f3a586032
cmdblock <wow328@163.com>
2014-10-31 08:12:23 +0900

    modification

--- vector_distance.m
+++ vector_distance.m
@@ -4,7 +4,7 @@ function [ output ] = vector_distance( vel,z1,z2)
     output=0;
     for i=1 : length(vel)-1
 %         output=output+calucate_distance(c(vel(1,i)),c(vel(1,i+1)),c);
-          output=output+sqrt((z1(i)-z1(i+1))^2+(z2(i)-z2(i+1))^2);
+          output=output+sqrt((z1(vel(1,i))-z1(vel(1,i+1)))^2+(z2(vel(1,i))-z2(vel(1,i+1)))^2);
     end
 
 end
